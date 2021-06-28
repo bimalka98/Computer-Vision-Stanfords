ae7ace835036616133591b5804599545b94918dd
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2021-06-29 03:24:22 +0530

    Initiated Nearest Neighbor(NN) and KNN

---
+++ CS231n/assignment1/cs231n/classifiers/__init__.py
@@ -0,0 +1,3 @@
+from .k_nearest_neighbor import *
+from .nearest_neighbor import *
+from .linear_classifier import *
---
+++ CS231n/assignment1/cs231n/classifiers/nearest_neighbor.py
@@ -0,0 +1,31 @@
+from builtins import range
+from builtins import object
+import numpy as np
+#from past.builtins import xrange
+
+class NearestNeighbor(object):
+  def __init__(self):
+    pass
+
+  def train(self, X, y):
+    """ X is N x D where each row is an example. Y is 1-dimension of size N """
+    # the nearest neighbor classifier simply remembers all the training data
+    self.Xtr = X
+    self.ytr = y
+
+  def predict(self, X):
+    """ X is N x D where each row is an example we wish to predict label for """
+    num_test = X.shape[0]
+    # lets make sure that the output type matches the input type
+    Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
+
+    # loop over all test rows
+    for i in range(num_test):
+      # find the nearest training image to the i'th test image
+      # using the L1 distance (sum of absolute value differences)
+      distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1) # L1 distance
+#       distances = np.sqrt(np.sum(np.square(self.Xtr - X[i,:]), axis = 1)) # L2 distance
+      min_index = np.argmin(distances) # get the index with smallest distance
+      Ypred[i] = self.ytr[min_index] # predict the label of the nearest example
+
+    return Ypred
